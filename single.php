971a04d3fa599be72c1a409e9b422ed04315261b
davidwolfpaw <davidwolfpaw@gmail.com>
2019-06-15 16:05:59 -0400

    updates schema for author details on single

--- single.php
+++ single.php
@@ -17,32 +17,23 @@ get_header(); ?>
 
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content', get_post_format() );
 
 			if ( true === get_theme_mod( 'author_info', true ) ) :
 				?>
-				<div class="author-info">
+				<div class="author-info author vcard h-card" itemprop="author" itemscope itemtype="http://schema.org/Person">
 					<div class="author-image">
 						<?php echo get_avatar( get_the_author_meta( 'ID' ), 96, 'mysteryman', get_the_author_meta( 'display_name' ) ); ?>
 					</div>
 					<div class="author-description">
-						<h4><?php echo esc_html( get_the_author_meta( 'display_name' ) ); ?></h4>
-						<p>
-							<?php
-							echo wp_kses(
-								get_the_author_meta( 'user_description' ),
-								array(
-									'a'      => array(
-										'href'  => array(),
-										'title' => array(),
-									),
-									'br'     => array(),
-									'em'     => array(),
-									'strong' => array(),
-								)
-							);
-							?>
-							</p>
+						<a class="url u-url fn p-fn n p-name" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo get_the_author(); ?>" rel="me author" itemprop="url">
+							<span itemprop="name"><?php echo get_the_author(); ?></span>
+						</a>
+						<?php if ( get_the_author_meta( 'description' ) ) { ?>
+							<div class="author-note note p-note" itemprop="description">
+								<p><?php echo wp_kses( get_the_author_meta( 'description' ), null ); ?></p>
+							</div>
+						<?php } ?>
 					</div>
 				</div><!-- .author-info -->
 				<?php
