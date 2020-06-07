45ccb380e82aed51d8a8d4acf782b871b78da7a9
David Wolfpaw <david@davidwolfpaw.com>
2020-06-07 13:56:44 -0400

    updates site-description to not be h2

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 					endif;
 					?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<?php
