db046480b4784cfefef8f1785fd079a68e8e7099
davidwolfpaw <david@davidwolfpaw.com>
2021-02-18 07:47:53 -0500

    removes aria-labelledby from menu modal

--- footer.php
+++ footer.php
@@ -57,7 +57,8 @@
 
 <div class="modal micromodal-slide" id="header-menu" aria-hidden="true">
 	<div class="modal-overlay" tabindex="-1" data-micromodal-close="">
-		<div class="modal-container" role="dialog" aria-modal="true" aria-labelledby="header-menu-title">
+		<div id="header-menu-title">Menu</div>
+		<div class="modal-container" role="dialog" aria-modal="true">
 			<header class="modal-header">
 				<button class="modal-close" aria-label="Close Menu" data-micromodal-close=""> Close Menu</button>
 			</header>
